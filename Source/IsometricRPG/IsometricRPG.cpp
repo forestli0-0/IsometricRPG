8629ce3fe10e90b53731865b880af88eb06b95a1
root <1529153250@qq.com>
2025-04-06 10:43:11 +0800

    添加项目文件。

---
+++ Source/IsometricRPG/IsometricRPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IsometricRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IsometricRPG, "IsometricRPG" );
