5334134d8a601562ebec6bb454b0bae2addf3eb4
root <1529153250@qq.com>
2025-05-01 23:02:36 +0800

    增强死亡能力和动画蓝图控制
    在 `GA_Death.cpp` 中添加对 `ABP_MyCharacterAnimInstance` 的引用，修改 `OnMontageEnded` 方法以暂停动画，并在 `ActivateAbility` 中设置角色死亡状态。
    
    在 `ActionQueueComponent.cpp` 中引入生命值检查，确保攻击目标存活。
    
    新增 `SetIsDead` 方法和 `bIsDead` 变量于 `ABP_MyCharacterAnimInstance`，以便在蓝图中控制角色的死亡状态。

---
+++ Source/IsometricRPG/Private/Animation/ABP_MyCharacterAnimInstance.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/ABP_MyCharacterAnimInstance.h"
+
+void UABP_MyCharacterAnimInstance::SetIsDead(bool NewValue)
+{
+    bIsDead = NewValue; // 直接设置C++变量
+}
