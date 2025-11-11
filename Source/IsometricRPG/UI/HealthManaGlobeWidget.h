a1a728a653dd08519c641d67fae17c7ec24a2ea9
root <1529153250@qq.com>
2025-11-11 15:48:17 +0800

    创建了状态条C++骨架

---
+++ Source/IsometricRPG/UI/HealthManaGlobeWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HealthManaGlobeWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ISOMETRICRPG_API UHealthManaGlobeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
