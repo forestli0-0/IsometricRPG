d9e0465ada7d1b6bfd36850ab8253ec71ad8335d
root <1529153250@qq.com>
2025-11-16 13:47:26 +0800

    添加调试日志以记录自我效果应用的角色名称和权限状态

--- Source/IsometricRPG/IsometricAbilities/GameplayAbilities/SelfCast/GA_HealRegenAbility.cpp
+++ Source/IsometricRPG/IsometricAbilities/GameplayAbilities/SelfCast/GA_HealRegenAbility.cpp
@@ -13,6 +13,12 @@ UGA_HealRegenAbility::UGA_HealRegenAbility()
 
 void UGA_HealRegenAbility::ApplySelfEffect(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
 {
+	const FString AvatarName = (ActorInfo && ActorInfo->AvatarActor.IsValid())
+		? ActorInfo->AvatarActor->GetName()
+		: TEXT("<None>");
+	UE_LOG(LogTemp, Log, TEXT("[PassiveDebug][RegenAbility] ApplySelfEffect on %s (Authority=%d)"),
+		*AvatarName,
+		ActorInfo ? ActorInfo->IsNetAuthority() : 0);
 	if (HealGameplayEffectClass)
 	{
 		auto ASC = ActorInfo->AbilitySystemComponent.Get();
